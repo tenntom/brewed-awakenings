a5ade5cd653ab8e2aae123994ceeba2ff118c3d7
Tom Clouse <tommyclouse@yahoo.com>
2021-04-23 09:55:27 -0500

    with price and number of products

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,6 +1,50 @@
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js"
+
+// document.addEventListener(
+//     "click",
+//     (clickEvent) => {           // function that takes place when the item is clicked.
+//         const itemClicked = clickEvent.target
+//         if (itemClicked.id.startsWith("employee")) {
+//             const [,employeeId] = itemClicked.id.split("--")
+           
+
+//             for (const employee of employees) {
+//                 if (employee.id === parseInt(employeeId)) {
+//                     const employeeOrders = orders.filter(orders.employeeId === employee.id);
+//                     window.alert(`${employee.name} sold ${employeeeOrders.length} products.`)
+//                 }
+//             }
+//         }
+//     }
+// )
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [, employeeId] = itemClicked.id.split("--") //don't 100% understand this step.
+
+            for (const employee of employees) {
+                if ( employee.id === parseInt(employeeId)) {
+
+                    const employeeOrders = orders.filter(  // storing matching values as true in array employeeOrders (I think)
+                        (order) => {
+                            if (order.employeeId === employee.id) {
+                                return true  
+                            }
+                        }
+                    )
+
+                    window.alert(` ${employee.name} sold ${employeeOrders.length} products `)
+                }
+            }
+        }
+    }
+)
 
 const employees = getEmployees()
+const orders = getOrders()
 
 export const Employees = () => {
     let html = "<ul>"
@@ -17,3 +61,5 @@ export const Employees = () => {
 //Copied from above
 // html += `<li>${employee.name}</li>`
 
+
+// const employeeArray = itemClicked.id.split("--")
--- scripts/Products.js
+++ scripts/Products.js
@@ -1,5 +1,22 @@
 import { getProducts } from "./database.js"
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("product")) {
+            const [, productId] = itemClicked.id.split("--")
+
+            for (const product of products) {
+                if (product.id === parseInt(productId)) {
+                    window.alert(`${product.name} costs $${product.price}`)
+                }
+            }
+        }
+    }
+)
+
+
 const products = getProducts()
 
 export const Products = () => {
@@ -13,4 +30,3 @@ export const Products = () => {
 
     return html
 }
-
